7c1b830abc604a7612f4a057c7c9e8bf80bb03e0
Tomek <tomo105@wp.pl>
2019-10-07 23:17:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firtstapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.firtstapp
+
+import android.os.Bundle
+import android.util.Log.d
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        bigButton.setOnClickListener {
+            d("tomek ", "Button was clieked !!")
+        }
+
+    }
+}
